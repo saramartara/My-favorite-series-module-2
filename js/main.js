05b30a35489634475dc47dc444c23cc2bd614269
saramartara <saramartinezaragon@gmail.com>
2021-02-08 18:58:06 +0100

    add searchSerie function to get correct data from api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log('evaluación final');
+const filterElement = document.querySelector('.js-filter');
+const searchElement = document.querySelector('.js-search');
+const formElement = document.querySelector('.js-form');
+
+const url = `http://api.tvmaze.com/search/shows?q=f`;
+let series = [];
+let favoriteSeries = [];
+
+//submit form
+
+function handleForm(ev) {
+  ev.preventDefault();
+}
+formElement.addEventListener('submit', handleForm);
+
+// search
+function handleSearch() {
+  let filterValue = filterElement.value;
+  console.log(filterValue);
+  searchSerie(filterValue);
+}
+
+function searchSerie(title) {
+  fetch(url + title)
+    .then((response) => response.json())
+    .then((data) => {
+      //   console.log(data[0].show);
+      for (const dataShow of data) {
+        series = dataShow;
+        console.log(dataShow);
+      }
+    });
+}
+
+// paint
+
+searchElement.addEventListener('click', handleSearch);
